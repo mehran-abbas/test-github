a1ad0a35e4c11545617191fc631fa2532e8ece60
Mehran Abbas <68163130+mehran-abbas@users.noreply.github.com>
2024-08-01 23:14:56 +0500

    Update Main.java

--- Helloworld/src/com/company/Main.java
+++ Helloworld/src/com/company/Main.java
@@ -3,13 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-       int a =9;
-        int b=0;
-        for (int i =0;i<=a;i++){
-            for(b =a;b>0;b--){
-                System.out.print("*")
+      
+     
+        for (int i =0;i<=20;i++){
+            for(int j = i;j >0 ;j--){
+                System.out.print("*");
             }
+            
+            System.out.println("");
         }
+         
 
     }
 }
