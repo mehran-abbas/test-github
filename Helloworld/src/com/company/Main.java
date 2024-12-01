18e1145eaea6eea7ff8c549bcb1f894bd8d0427b
Mehran Abbas <68163130+mehran-abbas@users.noreply.github.com>
2024-12-02 00:09:21 +0500

    Added a new method to draw a diamond shape
    Added a new method to draw a diamond shape according to size provided by user at run time (hard coded no ideal but works for now)

--- Helloworld/src/com/company/Main.java
+++ Helloworld/src/com/company/Main.java
@@ -1,18 +1,28 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-      
-     
-        for (int i =0;i<=20;i++){
-            for(int j = i;j >0 ;j--){
+        drawStar(10);
+    }
+
+    public static void drawStar(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
                 System.out.print("*");
             }
-            
-            System.out.println("");
+            System.out.println();
         }
-         
-
+        for (int i = size - 2; i >= 0; i--) {
+            for (int j = 0; j < size - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
